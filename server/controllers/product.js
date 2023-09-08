0b729b9bc447486f0c9348331827b38148f00fab
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-07 23:12:48 -0700

    fix style

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -48,72 +48,45 @@ module.exports = {
   },
 
   // gets the styles of a product from the database using the given product id query
-  async getProductStyles(req, res) {
-    if (req.query.product_id !== undefined) {
-    let styleObject = {
-      product_id: req.query.product_id,
-      results: []
-    };
-    const style = await pool.query(
-      `SELECT style.id as style_id, name, original_price, sale_price, default_style, photo.id as photo_id,
-      thumbnail_url, url,
-      sku.id as sku_id, sku.quantity as quantity, sku.size as size FROM style
-      INNER JOIN photo ON photo.id_style=style.id
-      INNER JOIN sku ON sku.id_style=style.id
-      WHERE id_product = ${req.query.product_id}
-      ORDER BY style.id ASC, photo.id ASC, sku.id ASC`
-    );
-
-    // to keep track of the style ID we are currently on
-    let lastUniqueStyleID = -1;
-    let lastUniquePhotoID = -1;
-    let lastUniqueSKUID = -1;
-
-    for (let i = 0; i < style.rows.length; i++) {
-
-      // create a new object on the first unique style id
-      if (lastUniqueStyleID !== style.rows[i].style_id) {
-      var newObj = {
-        style_id: style.rows[i].style_id,
-        name: style.rows[i].name,
-        original_price:  style.rows[i].original_price,
-        sale_price:  style.rows[i].sale_price,
-        'default?': style.rows[i].default_style,
-        photos: [],
-        skus: {}
-      };
-      lastUniqueStyleID = style.rows[i].style_id;
-    }
-
-      // if the photo id is unique, add photos to newObj
-      if (lastUniquePhotoID !== style.rows[i].photo_id) {
-        newObj['photos'].push({
-          thumbnail_url: style.rows[i].thumbnail_url,
-          url: style.rows[i].url
-        });
-        lastUniquePhotoID = style.rows[i].photo_id;
+   async getProductStyles(req, res) {
+      if (req.query.product_id !== undefined) {
+        let styleObject = {};
+        styleObject.product_id = req.query.product_id;
+        let results = [];
+        const style = await pool.query(`SELECT id as style_id, name, original_price, sale_price, default_style as "default?" FROM style WHERE id_product = ${req.query.product_id}`);
+  
+        for (let i = 0; i < style.rows.length; i++) {
+          let newObj = {...style.rows[i]};
+          let styleId = style.rows[i].style_id;
+          // add the photos for the style
+          const photo = await pool.query(`SELECT thumbnail_url, url FROM style LEFT JOIN photo ON photo.id_style=style.id WHERE id_style = ${styleId}`);
+          let photoArray = [];
+  
+          for (let y = 0; y < photo.rows.length; y++) {
+            let photoObj = {};
+            photoObj['thumbnail_url'] = photo.rows[y].thumbnail_url;
+            photoObj['url'] = photo.rows[y].url;
+            photoArray.push(photoObj);
+          }
+          newObj['photos'] = photoArray;
+          // add the sku quantity and size for the style
+          const sku = await pool.query(`SELECT sku.id, sku.quantity as quantity, sku.size as size FROM style LEFT JOIN sku ON sku.id_style=style.id WHERE id_style = ${styleId}`);
+          newObj['skus'] = {};
+          let skuObj = {};
+          for (let z = 0; z < sku.rows.length; z++) {
+            skuObj[`${sku.rows[z].id}`] = {
+              quantity: sku.rows[z].quantity,
+              size: sku.rows[z].size
+          };
+          newObj['skus'] = {...skuObj};
+          }
+          results.push(newObj);
+        }
+        styleObject.results = results;
+        res.send(styleObject);
+      } else {
+        res.sendStatus(500);
       }
-
-      // if the sku id is unique, add sku data to newObj
-      if (lastUniqueSKUID < style.rows[i].sku_id) {
-        newObj['skus'][`${style.rows[i].sku_id}`] = {
-          quantity: style.rows[i].quantity,
-          size: style.rows[i].size
-        };
-        lastUniqueSKUID = style.rows[i].sku_id;
-      }
-
-      // if the next style id is unique, push newObj to the results
-      if (i+1 < style.rows.length && lastUniqueStyleID !== style.rows[i+1].style_id) {
-        styleObject['results'].push(newObj);
-      }
-    }
-    // push the last newObj to results
-    styleObject['results'].push(newObj);
-    res.send(styleObject);
-  } else {
-    res.sendStatus(500);
-  }
   },
 
   // gets array of related product IDs
