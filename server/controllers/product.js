16dcce5b7e09d333157b33bbb0d3974bd0c8db09
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-08 19:04:38 -0700

    add testing endpoints for random last 10%

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,6 +1,96 @@
 const {pool} = require('../db.js');
 
 module.exports = {
+  // to test different endpoints due to limitations of loader.io free tier
+  async getProductsTest(req, res) {
+    if (req.query.product_id !== undefined) {
+    const min = 900010;
+    const max = 1000011;
+    const randProduct = Math.floor(Math.random() * (max - min + 1) + min);
+    const product = await pool.query(
+      `SELECT * FROM product
+      INNER JOIN feature on feature.id_product=product.id
+      WHERE product.id=${randProduct}`);
+      let productObj = {
+        id: product.rows[0].id,
+        name: product.rows[0].name,
+        slogan: product.rows[0].slogan,
+        description: product.rows[0].description,
+        category: product.rows[0].category,
+        default_price: product.rows[0].default_price,
+        features: []
+      };
+      for (let i = 0; i < product.rows.length; i++) {
+        productObj['features'].push({
+          feature: product.rows[i].feature,
+          value: product.rows[i].value
+        });
+      }
+      res.send(productObj);
+    } else {
+      res.sendStatus(500);
+    }
+  },
+    // to test different endpoints due to limitations of loader.io free tier
+  async getStylesTest(req, res) {
+    if (req.query.product_id !== undefined) {
+    const min = 900010;
+    const max = 1000011;
+    const randProduct = Math.floor(Math.random() * (max - min + 1) + min);
+    let styleObject = {};
+    styleObject.product_id = randProduct;
+    let results = [];
+    const style = await pool.query(`SELECT id as style_id, name, original_price, sale_price, default_style as "default?" FROM style WHERE id_product = ${randProduct}`);
+
+    for (let i = 0; i < style.rows.length; i++) {
+      let newObj = {...style.rows[i]};
+      let styleId = style.rows[i].style_id;
+      // add the photos for the style
+      const photo = await pool.query(`SELECT thumbnail_url, url FROM style LEFT JOIN photo ON photo.id_style=style.id WHERE id_style = ${styleId}`);
+      let photoArray = [];
+
+      for (let y = 0; y < photo.rows.length; y++) {
+        let photoObj = {};
+        photoObj['thumbnail_url'] = photo.rows[y].thumbnail_url;
+        photoObj['url'] = photo.rows[y].url;
+        photoArray.push(photoObj);
+      }
+      newObj['photos'] = photoArray;
+      // add the sku quantity and size for the style
+      const sku = await pool.query(`SELECT sku.id, sku.quantity as quantity, sku.size as size FROM style LEFT JOIN sku ON sku.id_style=style.id WHERE id_style = ${styleId}`);
+      newObj['skus'] = {};
+      let skuObj = {};
+      for (let z = 0; z < sku.rows.length; z++) {
+        skuObj[`${sku.rows[z].id}`] = {
+          quantity: sku.rows[z].quantity,
+          size: sku.rows[z].size
+      };
+      newObj['skus'] = {...skuObj};
+      }
+      results.push(newObj);
+    }
+    styleObject.results = results;
+    res.send(styleObject);
+  } else {
+    res.sendStatus(500);
+  }
+  },
+ // to test different endpoints due to limitations of loader.io free tier
+    async getRelatedTest(req, res) {
+      if (req.query.product_id !== undefined) {
+        const min = 209383;
+        const max = 232648;
+        const randProduct = Math.floor(Math.random() * (max - min + 1) + min);
+        const related = await pool.query(`SELECT related_products.related_product_id FROM product LEFT JOIN related_products ON related_products.current_product_id=product.id WHERE product.id=${randProduct}`);
+        let relatedIDs = [];
+        for (let i = 0; i < related.rows.length; i++) {
+          relatedIDs.push(related.rows[i]['related_product_id']);
+        }
+        res.send(relatedIDs);
+      } else {
+        res.sendStatus(500);
+      }
+    },
   // gets one product from the database using the given product id parameter or gets all products if no product id parameter
   async getProducts(req, res) {
     // if product id query parameter is given, return the one product
@@ -54,14 +144,14 @@ module.exports = {
         styleObject.product_id = req.query.product_id;
         let results = [];
         const style = await pool.query(`SELECT id as style_id, name, original_price, sale_price, default_style as "default?" FROM style WHERE id_product = ${req.query.product_id}`);
-  
+
         for (let i = 0; i < style.rows.length; i++) {
           let newObj = {...style.rows[i]};
           let styleId = style.rows[i].style_id;
           // add the photos for the style
           const photo = await pool.query(`SELECT thumbnail_url, url FROM style LEFT JOIN photo ON photo.id_style=style.id WHERE id_style = ${styleId}`);
           let photoArray = [];
-  
+
           for (let y = 0; y < photo.rows.length; y++) {
             let photoObj = {};
             photoObj['thumbnail_url'] = photo.rows[y].thumbnail_url;
--- server/router.js
+++ server/router.js
@@ -6,5 +6,8 @@ const db = require('./db.js')
 router.get('/products', controller.products.getProducts);
 router.get('/products/styles', controller.products.getProductStyles);
 router.get('/products/related', controller.products.getRelatedProducts);
+router.get('/products/test', controller.products.getProductsTest);
+router.get('/products/styles/test', controller.products.getStylesTest);
+router.get('/products/related/test', controller.products.getRelatedTest);
 
 module.exports = router;
