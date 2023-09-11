6f11a0d425fdf9abbfdeadee4f3795db9bd51b60
Kimberly Tom <kimberlyjtom@gmail.com>
2023-09-11 09:56:39 -0700

    remove test code

--- server/db.js
+++ server/db.js
@@ -13,29 +13,20 @@ dotenv.config();
         pool.connect();
 
 
-//   const redis = new Redis( {host:process.env.REDISHOST, port:6379, connectTimeout: 10000 } );
-// redis.on('connect', () => {
-//   console.log('Redis client is initiating a connection to the server.');
-// });
+const redis = new Redis( {host:process.env.REDISHOST, port:6379, connectTimeout: 10000 } );
+redis.on('connect', () => {
+  console.log('Redis client is connecting.');
+});
 
-// redis.on('ready', () => {
-//   console.log('Redis client successfully initiated connection to the server.');
-// });
+redis.on('ready', () => {
+  console.log('Redis client is connected.');
+});
 
-// redis.on('reconnecting', () => {
-//   console.log('Redis client is trying to reconnect to the server...');
-// });
+redis.on('reconnecting', () => {
+  console.log('Redis client is reconnecting.');
+});
 
-// redis.on('error', (err) => console.log('Redis Client Error', err));
-
-// //check the functioning
-// redis.set("framework", "AngularJS", function(err, reply) {
-//   console.log("redis.set ", reply);
-// });
-
-// redis.get("framework", function(err, reply) {
-//   console.log("redis.get ", reply);
-// });
+redis.on('error', (err) => console.log('Redis client has error', err));
 
 exports.pool = pool;
 // exports.redis = redis;
